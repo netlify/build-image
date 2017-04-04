1fab05001b3e5e63e48f14f5617056a3c09299c3
David Calavera <david.calavera@gmail.com>
2017-04-04 11:07:05 -0700

    Improvements to run-build.sh for more custom workflows.
    This allows people to set custom Yarn versions and have
    them install them on demand.
    
    Signed-off-by: David Calavera <david.calavera@gmail.com>

--- run-build.sh
+++ run-build.sh
@@ -1,120 +1,42 @@
 #!/bin/bash
 
-export CMD=$*
+dir="$(dirname "$0")"
 
-if [[ -z "$CMD" ]]; then
-  echo "Usage: $0 <your build cmd>"
-  exit 1
+: ${NETLIFY_REPO_URL="/opt/repo"}
+
+NETLIFY_BUILD_BASE="/opt/buildhome"
+NETLIFY_CACHE_DIR="$NETLIFY_BUILD_BASE/cache"
+NETLIFY_REPO_DIR="$NETLIFY_BUILD_BASE/repo"
+
+cmd=$*
+
+BUILD_COMMAND_PARSER=$(cat <<EOF
+$cmd
+EOF
+)
+
+mkdir -p $NETLIFY_CACHE_DIR
+rm -rf $NETLIFY_BUILD_BASE/.yarn
+
+cd $NETLIFY_BUILD_BASE
+if [[ ! -d $NETLIFY_REPO_DIR ]]; then
+  git clone $NETLIFY_REPO_URL $NETLIFY_REPO_DIR
 fi
+cd $NETLIFY_REPO_DIR
 
-set -e
+. "$dir/run-build-functions.sh"
 
-cd /opt/buildhome
-if [[ ! -d repo ]]; then
-  git clone /opt/repo repo
-fi
-cd repo
+echo "Installing dependencies"
+install_dependencies $NODE_VERSION $RUBY_VERSION $YARN_VERSION
 
-if [[ -f runtime.txt ]]; then
-	if source $HOME/python$(cat runtime.txt)/bin/activate; then
-		echo "Python version set to $(cat runtime.txt)"
-	else
-		echo "Error setting python version from runtime.txt"
-		echo "Will use default version (2.7)"
-		source $HOME/python2.7/bin/activate
-	fi
-else
-	source $HOME/python2.7/bin/activate
-fi
+echo "Installing missing commands"
+install_missing_commands
 
-# Node version
-source $HOME/.nvm/nvm.sh
-export NODE_VERSION=6
-if [[ -f .nvmrc ]]; then
-  nvm install $(cat .nvmrc)
-  if [[ $? -ne 0 ]]; then
-    echo "Failed to set version of node to '$(cat .nvmrc)' from .nvmrc. Falling back to version $NODE_VERSION"
-  else
-    NODE_VERSION=$(nvm current)
-  fi
-fi
-export NODE_VERSION=$NODE_VERSION
+echo "Executing user command: $cmd"
+`$cmd` > out.log 2> err.log
+CODE=$?
 
-# Ruby version
-source $HOME/.rvm/scripts/rvm
-export RUBY_VERSION=2.1.2
-if [[ -f .ruby-version ]]; then
-	if rvm use $(cat .ruby-version); then
-		echo "Set ruby from .ruby-version"
-		export RUBY_VERSION=$(cat .ruby-version)
-	else
-		echo "Error setting ruby version from .ruby-version file. Unsupported version?"
-		echo "Will use default version (2.1.2)"
-		rvm use $RUBY_VERSION
-	fi
-else
-	rvm use $RUBY_VERSION
-fi
+echo "Caching artifacts"
+cache_artifacts
 
-# Java version
-export JAVA_VERSION=default_sdk
-
-# Rubygems
-if [[ -f Gemfile ]]; then
-  echo "Installing gem bundle"
-  if bundle install --path $HOME/bundle --deployment --binstubs=$HOME/binstubs; then
-		export PATH=$HOME/binstubs:$PATH
-		echo "Gem bundle installed"
-	fi
-fi
-
-# PIP dependencies
-if [[ -f requirements.txt ]]; then
-	echo "Installing pip dependencies"
-	if pip install -r requirements.txt; then
-		echo "Pip dependencies installed"
-	fi
-fi
-
-# NPM Dependencies
-if [[ -f package.json ]]; then
-	echo "Installing npm modules"
-	if [[ -f yarn.lock ]]; then
-		if yarn install; then
-			export PATH=$(yarn bin):$PATH
-			echo "NPM modules installed"
-		fi
-	else
-		if npm install; then
-			export PATH=$(npm bin):$PATH
-			echo "NPM modules installed"
-		fi
-	fi
-fi
-
-# Bower Dependencies
-if [[ -f bower.json ]]; then
-	echo "Installing bower components"
-	if bower install --config.interactive=false; then
-		echo "Bower components installed"
-	fi
-fi
-
-# Leiningen
-if [[ -f project.clj ]]; then
-	if lein deps; then
-		echo "Leiningen dependencies installed"
-	fi
-fi
-
-# Hugo
-if [[ -n "$HUGO_VERSION" ]]; then
-	hugoPath=$(binrc install hugo)
-	if [[ $? -eq 0 ]]; then
-		export PATH=$(dirname $hugoPath):$PATH
-		echo "Hugo ${HUGO_VERSION} installed"
-	fi
-fi
-
-echo "Running build command '$CMD'"
-eval $CMD
+exit $CODE

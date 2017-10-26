cd9bbfa4c3013a3f59598c9bba820f5ab4056351
xcsrz <x@xcsrz.me>
2017-10-26 10:23:22 -0700

    fixes for phpbrew install (#95) and libvips install (#92)

--- test-tools/start-image.sh
+++ test-tools/start-image.sh
@@ -8,6 +8,7 @@ docker run --rm -t -i \
 	-e NPM_VERSION \
 	-e RUBY_VERSION \
 	-e YARN_VERSION \
+    -e HUGO_VERSION \
 	-v ${REPO_PATH}:/opt/repo \
 	-v ${BASE_PATH}/run-build.sh:/usr/local/bin/build \
 	-v ${BASE_PATH}/run-build-functions.sh:/usr/local/bin/run-build-functions.sh \

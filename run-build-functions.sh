b0a539a06796e72d4c6803cc1542af581889b674
Arnaud Ligny <arnaud@ligny.org>
2017-11-14 00:42:16 +0100

    Update run-build-functions.sh
    Should not cache `vendor`.

--- run-build-functions.sh
+++ run-build-functions.sh
@@ -397,10 +397,6 @@ install_dependencies() {
     then
       mv $NETLIFY_CACHE_DIR/.composer $HOME/.composer
     fi
-    if [ -d $NETLIFY_CACHE_DIR/vendor ]
-    then
-      mv $NETLIFY_CACHE_DIR/vendor $NETLIFY_REPO_DIR/vendor
-    fi
     composer install
   fi
 }
@@ -463,14 +459,6 @@ cache_artifacts() {
     rm -rf $NETLIFY_CACHE_DIR/.composer
       mv $HOME/.composer $NETLIFY_CACHE_DIR/.composer
       echo "Saved Composer Directory"
-  fi
-
-  # Cache vendor
-  if [ -d $NETLIFY_REPO_DIR/vendor ]
-  then
-    rm -rf $NETLIFY_CACHE_DIR/vendor
-      mv $NETLIFY_REPO_DIR/vendor $NETLIFY_CACHE_DIR/vendor
-      echo "Saved Vendor Directory"
   fi
 }
 

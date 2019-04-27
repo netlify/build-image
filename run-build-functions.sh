2cbef97a958b56e95179a0c42de881ce32ccfc52
Bret Comnes <bcomnes@gmail.com>
2019-04-26 17:23:02 -0700

    Link the custom version of ZISI

--- run-build-functions.sh
+++ run-build-functions.sh
@@ -461,6 +461,24 @@ install_dependencies() {
     fi
   fi
 
+  # zip-it-and-ship-it
+  if [ -n "$ZISI_VERSION" ]
+  then
+    echo "Installing Zip-it-and-ship-it $ZISI_VERSION"
+
+    zisiOut=$(binrc install -c $NETLIFY_BUILD_BASE/.binrc netlify/zip-it-and-ship-it $ZISI_VERSION)
+    if [ $? -eq 0 ]
+    then
+      echo $zisiOut
+      ln -s $zisiOut /opt/buildhome/.binrc/bin/zip-it-and-ship-it_${ZISI_VERSION}
+      ln -s /opt/buildhome/.binrc/bin/zip-it-and-ship-it_${ZISI_VERSION} /opt/buildhome/.binrc/bin/zip-it-and-ship-it
+      zip-it-and-ship-it --version
+    else
+      echo "Error during Zip-it-and-ship-it $ZISI_VERSION install: $zisiOut"
+      exit 1
+    fi
+  fi
+
   # Cask
   if [ -f Cask ]
   then

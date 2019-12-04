47d7346dea865a90ca9fd81db5c7ce5e1a978b11
Dan (Turk) Osborne <dan@projectcalico.org>
2019-12-04 15:00:50 -0800

    start-image.sh should launch correct image on trusty

--- test-tools/start-image.sh
+++ test-tools/start-image.sh
@@ -14,4 +14,4 @@ docker run --rm -t -i \
 	-v ${REPO_PATH}:/opt/repo \
 	-v ${BASE_PATH}/run-build.sh:/usr/local/bin/build \
 	-v ${BASE_PATH}/run-build-functions.sh:/usr/local/bin/run-build-functions.sh \
-	netlify/build /bin/bash
+	netlify/build:trusty /bin/bash

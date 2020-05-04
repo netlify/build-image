b4309a004e10e3e8dfb18b851f540a31b5b2cfac
Marcus Weiner <marcus.weiner@gmail.com>
2020-05-04 02:08:21 +0200

    Move brew install before all other dependencies
    Mainly for allowing native node modules to use homebrew stuff

--- run-build-functions.sh
+++ run-build-functions.sh
@@ -434,6 +434,12 @@ install_dependencies() {
     exit 1
   fi
 
+  # Homebrew from Brewfile
+  if [ -f Brewfile ] || [ ! -z "$HOMEBREW_BUNDLE_FILE" ]
+  then
+    brew bundle
+  fi
+
   # SPM dependencies
   if [ -f Package.swift ]
   then
@@ -662,12 +668,6 @@ install_dependencies() {
     mkdir -p "$(dirname $GOPATH/src/$GO_IMPORT_PATH)"
     rm -rf $GOPATH/src/$GO_IMPORT_PATH
     ln -s /opt/buildhome/repo ${GOPATH}/src/$GO_IMPORT_PATH
-  fi
-
-  # Homebrew from Brewfile
-  if [ -f Brewfile ] || [ ! -z "$HOMEBREW_BUNDLE_FILE" ]
-  then
-    brew bundle
   fi
 }
 

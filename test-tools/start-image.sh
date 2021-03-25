249283e78b19911a71b60cb0dfbb09e2ba115f64
Hraban Luyat <hraban@0brg.net>
2021-03-25 23:44:32 +0000

    syntax: bash launcher script hardening
    Apply various hardening idioms to the default Bash launcher script:
    
    - quote all variables
    - quote all path names
    - explicit error message on wrong usage
    - use exec for the "wrapped" command
    - use common hardening flags
    - use /usr/bin/env

--- test-tools/start-image.sh
+++ test-tools/start-image.sh
@@ -1,10 +1,18 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-BASE_PATH=$(pwd)
-REPO_PATH=$(cd $1 && pwd)
+set -eu -o pipefail
+
+repo="${1-}"
+if [[ -z "$repo" ]]; then
+	>&2 echo "Usage: $0 <YOUR SITE REPO>"
+	exit 1
+fi
+
+BASE_PATH="$(pwd)"
+REPO_PATH="$(cd "$repo" && pwd)"
 : ${NETLIFY_IMAGE="netlify/build:xenial"}
 
-docker run --rm -t -i \
+exec docker run --rm -t -i \
 	-e NODE_VERSION \
 	-e NPM_VERSION \
 	-e RUBY_VERSION \
@@ -14,7 +22,7 @@ docker run --rm -t -i \
 	-e GO_VERSION \
 	-e SWIFT_VERSION \
 	-e PYTHON_VERSION \
-	-v ${REPO_PATH}:/opt/repo \
-	-v ${BASE_PATH}/run-build.sh:/opt/build-bin/build \
-	-v ${BASE_PATH}/run-build-functions.sh:/opt/build-bin/run-build-functions.sh \
-	$NETLIFY_IMAGE /bin/bash
+	-v "${REPO_PATH}":/opt/repo \
+	-v "${BASE_PATH}"/run-build.sh:/opt/build-bin/build \
+	-v "${BASE_PATH}"/run-build-functions.sh:/opt/build-bin/run-build-functions.sh \
+	"$NETLIFY_IMAGE" /bin/bash

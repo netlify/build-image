f2d2b9c5b5dd69c09ba2ab7592d8a95217cf2b89
Jacob Maine <jacob.maine@gmail.com>
2021-04-29 09:40:26 -0700

    Cache Clojure git dependencies
    The [Clojure CLI tools][clojure-cli] have a mechanism of downloading
    dependencies directly from git repositories rather than from a package
    manager. This patch caches the directory which stores these downloads,
    speeding up builds of repos that use these types of dependencies.
    
    [clojure-cli]: https://clojure.org/guides/deps_and_cli

--- run-build-functions.sh
+++ run-build-functions.sh
@@ -585,6 +585,7 @@ install_dependencies() {
   if [ -f deps.edn ]
   then
     restore_home_cache ".m2" "maven dependencies"
+    restore_home_cache ".gitlibs" "clojure git dependencies"
     restore_cwd_cache ".cpcache" "clojure classpath"
     echo "Installing Clojure dependencies"
     if clojure -Spath -Sforce >/dev/null
@@ -730,6 +731,7 @@ cache_artifacts() {
   cache_home_directory ".emacs.d" "emacs cache"
   cache_home_directory ".m2" "maven dependencies"
   cache_home_directory ".boot" "boot dependencies"
+  cache_home_directory ".gitlibs" "clojure git dependencies"
   cache_home_directory ".composer" "composer dependencies"
   cache_home_directory ".homebrew-cache", "homebrew cache"
   cache_home_directory ".rustup" "rust rustup cache"

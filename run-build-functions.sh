c954246e517c8996ef9a7429f7e77003839f8967
Benaiah Mischenko <benaiah@mischenko.com>
2021-05-05 15:22:29 -0700

    chore: remove duplicated go import path code in Focal image (#565)
    * fix: don't remove siblings of the go import path we're adding
    
    * fix: remove duplicated code
    
    * fix: ensure the parent directory exists before symlinking

--- run-build-functions.sh
+++ run-build-functions.sh
@@ -687,14 +687,6 @@ install_dependencies() {
     restore_cwd_cache "target" "rust compile output"
     source $HOME/.cargo/env
   fi
-
-  # Setup project GOPATH
-  if [ -n "$GO_IMPORT_PATH" ]
-  then
-    mkdir -p "$(dirname $GOPATH/src/$GO_IMPORT_PATH)"
-    rm -rf $GOPATH/src/$GO_IMPORT_PATH
-    ln -s /opt/buildhome/repo ${GOPATH}/src/$GO_IMPORT_PATH
-  fi
 }
 
 #
@@ -890,7 +882,7 @@ set_go_import_path() {
     local importPath="$GOPATH/src/$GO_IMPORT_PATH"
     local dirPath="$(dirname $importPath)"
 
-    rm -rf $dirPath
+    rm -rf $importPath
     mkdir -p $dirPath
     ln -s $PWD $importPath
 

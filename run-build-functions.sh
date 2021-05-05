a5daf5130c3326766b28f316e2deb2c2e65e96bf
João Antunes <joao@netlify.com>
2021-05-05 15:19:14 +0100

    fix(workspace): don't log workspace detection errors from stderr (#568)

--- run-build-functions.sh
+++ run-build-functions.sh
@@ -112,7 +112,7 @@ run_yarn() {
   local workspace_exit_code
   # YARN_IGNORE_PATH will ignore the presence of a local yarn executable (i.e. yarn 2) and default
   # to using the global one (which, for now, is always yarn 1.x). See https://yarnpkg.com/configuration/yarnrc#ignorePath
-  workspace_output="$(YARN_IGNORE_PATH=1 yarn workspaces --json info )"
+  workspace_output="$(YARN_IGNORE_PATH=1 yarn workspaces --json info 2>/dev/null)"
   workspace_exit_code=$?
   if [ $workspace_exit_code -eq 0 ]
   then

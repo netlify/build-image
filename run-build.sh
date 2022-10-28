bfd2c31073c336a817acc193d62eb32f1217cd6d
Lukas Holzer <lukas.holzer@netlify.com>
2022-10-28 11:09:29 +0200

    chore: updates

--- run-build.sh
+++ run-build.sh
@@ -16,28 +16,33 @@ EOF
 if [[ ! -d $NETLIFY_REPO_DIR ]]; then
   git clone $NETLIFY_REPO_URL $NETLIFY_REPO_DIR
 fi
-cd $NETLIFY_REPO_DIR
 
-: ${NODE_VERSION="16"}
-: ${RUBY_VERSION="2.7.2"}
-: ${YARN_VERSION="1.22.19"}
-: ${PNPM_VERSION="7.13.4"}
-: ${GO_VERSION="1.17"}
-: ${PYTHON_VERSION="3.8"}
+cd "$NETLIFY_REPO_DIR" || exit
+
+: "${NODE_VERSION="16"}"
+: "${RUBY_VERSION="2.7.2"}"
+: "${YARN_VERSION="1.22.19"}"
+: "${PNPM_VERSION="7.13.4"}"
+: "${GO_VERSION="1.17"}"
+: "${PYTHON_VERSION="3.8"}"
+: "${BUILD_INFO="$(node tests/get-build-info.mjs)"}"
+: "${FEATURE_FLAGS="build-image_use_new_package_manager_detection"}"
+
+echo "$BUILD_INFO"
 
 echo "Installing dependencies"
-install_dependencies $NODE_VERSION $RUBY_VERSION $YARN_VERSION $PNPM_VERSION $GO_VERSION $PYTHON_VERSION
+# install_dependencies "$NODE_VERSION" "$RUBY_VERSION" "$YARN_VERSION" "$PNPM_VERSION" "$GO_VERSION" "$PYTHON_VERSION" "$BUILD_INFO" "$FEATURE_FLAGS"
 
-echo "Installing missing commands"
-install_missing_commands
+# echo "Installing missing commands"
+# install_missing_commands
 
-echo "Verify run directory"
-set_go_import_path
+# echo "Verify run directory"
+# set_go_import_path
 
-echo "Executing user command: $cmd"
-eval "$cmd"
-CODE=$?
+# echo "Executing user command: $cmd"
+# eval "$cmd"
+# CODE=$?
 
-cache_artifacts
+# cache_artifacts
 
 exit $CODE

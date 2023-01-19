93cc58c28d0cd056ccfb2c5937105074d6c5a8fb
jobala <japhethobalak@gmail.com>
2023-01-19 20:24:34 +0000

    chore: source nvm

--- run-build-functions.sh
+++ run-build-functions.sh
@@ -45,6 +45,8 @@ mkdir -p $NETLIFY_CACHE_DIR/node_version
 mkdir -p $NETLIFY_CACHE_DIR/ruby_version
 mkdir -p $NETLIFY_CACHE_DIR/swift_version
 
+source $NVM_DIR/nvm.sh
+
 # pwd caches
 NETLIFY_JS_WORKSPACES_CACHE_DIR="$NETLIFY_CACHE_DIR/js-workspaces"
 
@@ -297,7 +299,6 @@ install_node() {
   local featureFlags=$2
 
 
-  source $NVM_DIR/nvm.sh
   : ${NODE_VERSION="$defaultNodeVersion"}
 
   # restore only non-existing cached versions
